023038be733469f722c3736d005d5829affcc656
Jorge Perez <jorgeperez_1@itesm.mx>
2020-11-10 12:12:40 -0600

    Abers si completa el venv

--- Code/ManageData.py
+++ Code/ManageData.py
@@ -1,4 +1,6 @@
 import random
+from MySQLCnnt import DB
+
 
 def createUser(db: object, name: str, lastName: str, age: int, 
                 excercise: int, profilePic: str, avergage_SaO2: int):
@@ -18,19 +20,22 @@ def insert_User(db: object, data: list):
 
 def inserReading_HR(db: object, data: list):
     db.insertData("HR_Readings", ["User", "Time", "HR"], data)
-    pass
 
 def inserReading_SaO2(db: object, data: list):
     db.insertData("SaO2_Readings", ["User", "Time", "SaO2"], data)
-    pass
 
-def sampleData_HR(db: object, data: list):
-    return random.randint(60, 100)
+def sampleData_HR():
+    return [ random.randint(0, 5), sampleData_Time(), random.randint(60, 100)]
 
 def sampleData_Time():
-    return
+    return "2020/"+ random.randint(1, 12)+ "/" + random.randint(1, 28)
 
 def sampleData_SaO2():
-    return random.randint(85, 100)
+    return [ random.randint(0, 5), sampleData_Time(), random.randint(75, 100)]
     
 
+db =  DB("IoT", save = True)
+
+for _ in range(0,1000):
+    inserReading_HR(db, sampleData_HR())
+    inserReading_SaO2(db, sampleData_SaO2())
--- Code/main.py
+++ Code/main.py
@@ -1,19 +1,45 @@
 # Proyecto desarrollado por:
     # Jorge Pérez - A01023859
     # Luis Fernández - A01023675
-    # Josué Mojica
+    # Josué Mojica - A01024035
 ## ITESM CSF Semestre Ago - Dic 2020s
 
-from MySQLCnnt import DB
 
-db =  DB("IoT", save = False)
 
-db.insertData("users", ["id_person", "name", "birth"], [4, "Octavio", "1985-02-28"])
 
-db.insertData("users", ["name", "birth"], ["Francisco", "1980-04-28"])
 
-results = db.getQuerryRes("SELECT * FROM users")
-# alter table auto_increment = #
-for result in results:
-    print(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from MySQLCnnt import DB
+
+# db =  DB("IoT", save = False)
+
+# db.insertData("users", ["id_person", "name", "birth"], [4, "Octavio", "1985-02-28"])
+
+# db.insertData("users", ["name", "birth"], ["Francisco", "1980-04-28"])
+
+# results = db.getQuerryRes("SELECT * FROM users")
+# # alter table auto_increment = #
+# for result in results:
+#     print(result)
 
